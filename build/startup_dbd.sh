c04a1f1377647a160c51c31881912cb508dbb814
Matt S <matt.shepit@csiro.au>
2024-01-18 17:23:43 +1000

    split slurmdbd into its own container, might be working?

--- build/startup_dbd.sh
+++ build/startup_dbd.sh
@@ -10,12 +10,7 @@ fi
 # Configure Slurm to use maximum available processors and memory
 # and start required services
 ${sudo_cmd} bash <<SCRIPT
-sed -i "s/<<HOSTNAME>>/$(hostname)/" /etc/slurm/slurm.conf
-sed -i "s/<<CPU>>/$(nproc)/" /etc/slurm/slurm.conf
-sed -i "s/<<MEMORY>>/$(if [[ "$(slurmd -C)" =~ RealMemory=([0-9]+) ]]; then echo "${BASH_REMATCH[1]}"; else exit 100; fi)/" /etc/slurm/slurm.conf
 service munge start
-service slurmd start
-service slurmctld start
 service slurmdbd start
 SCRIPT
 
